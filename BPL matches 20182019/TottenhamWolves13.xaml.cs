add5452ae13055c52e5352b7d086da75961fe02c
Dawid Janczyk <45544015+DawidJanczyk@users.noreply.github.com>
2019-04-05 13:35:36 +0200

    Add files via upload

---
+++ BPL matches 20182019/TottenhamWolves13.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Football_History.Photos.Matches.BPL_matches.Matches_2019_2018
+{
+    /// <summary>
+    /// Logika interakcji dla klasy TottenhamWolves13.xaml
+    /// </summary>
+    public partial class TottenhamWolves13 : Window
+    {
+        public TottenhamWolves13()
+        {
+            InitializeComponent();
+        }
+    }
+}
